e276f7083fdca6e02cd7fabe420a9a00394a4bfd
Medonca09 <lucasmm0908@gmail.com>
2024-01-15 12:36:38 -0300

    Estilizando o footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react';
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize, Maximize2 } from 'lucide-react';
 
 export default function Home() {
 	return (
@@ -123,16 +123,47 @@ export default function Home() {
 					</div>
 				</main> 
 			</div>
-			<footer className="bg-zinc-800 border-t border-zinc-700 px-6 flex items-center justify-between">footer</footer>
-			<div>
-				
+			<footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+			<div className='flex items-center gap-3'>
+			<img src='/album.png' width={56} height={56} alt='capa álbum Utopia'/>
+			<div className='flex flex-col'>
+				<strong className='font-normal'>FE!N(feat.Playboi Carti)</strong>
+				<span className='text-xs text-zinc-400'>Travis Scott, Playboi Carti</span>
 			</div>
-			<div>
-				
 			</div>
-			<div>
-				
+			<div className='flex flex-col items-center gap-2'>
+				<div className='flex items-center gap-6'>
+					<Shuffle size={20} className='text-zinc-200'/>
+					<SkipBack size={20} className='text-zinc-200'/>
+
+					<button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black'>
+						<Play />
+					</button>
+
+					<SkipForward size={20} className='text-zinc-200'/>
+					<Repeat size={20} className='text-zinc-200'/>
+				</div>
+				<div className='flex items-center gap-2'>
+					<span className="text-xs text-zinc-400">1:24</span>
+					<div className='h-1 rounded-full w-96 bg-zinc-600'>
+						<div className='bg-zinc-200 w-40 h-1 rounded-full hover:bg-green-600 transition-colors'></div>
+					</div>
+					<span className="text-xs text-zinc-400">3:11</span>
+				</div>
 			</div>
+			<div className='flex items-center gap-4'>
+				<Mic2 size={20}/>
+				<LayoutList size={20}/>
+				<Laptop2 size={20}/>
+				<div className='flex items-center gap-2'>
+					<Volume size={20}/>
+					<div className='h-1 rounded-full w-24 bg-zinc-600'>
+						<div className='bg-zinc-200 w-10 h-1 rounded-full hover:bg-green-600 transition-colors'></div>
+					</div>
+				</div>
+				<Maximize2 size={20}/>
+			</div>
+			</footer>
 		</div>
 	);
 }
